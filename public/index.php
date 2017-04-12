df43e6b03b99ef68131263f3baab29db948c663f
avravikiran-sopho <avenkataravikiran@gmail.com>
2017-04-12 17:21:12 +0000

    changes

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-    
+    ini_set('max_execution_time', 3000);
     function render($view, $values = [])
     {
         // if view exists, render it
@@ -36,31 +36,34 @@
         $regex1 = '/instituteContainer(.*?)Download Brochure/s';
         $regex2 ='/tuple-clg-heading"\>(?:.*?)"\>(.*?)\<\/a\>(?:.*?)\>\|(.*?)\<\/p\>/s';
         $regex3 = '/\<h3\>(.*?)\<\/h3\>/s';
-        $regex4 ='/data\-page/s';
+        $regex4 ='/next linkpagination/s';
         $regex5 ='/tuple-revw-sec(?:.*?)\<b\>(.*?)\<\/b\>/s';
-        $regex6 ='/\-\d/s';
+        $regex6 ='/\-[0-9].*/s';
         $regex7 ='/\/|\-/';
         
         $url = $_GET["input"];
-        $html = getHTML ($url,30);
-        
-        
-        preg_match_all ($regex4,$html,$pagination);
-        
-        $pages = sizeof($pagination[0]);
-        //print ($pages);
-        
+        $pages = 1;
         $alldata='';
-        for ($count=1;$count<=$pages;$count++) {
-            //$change = '';
-            //$url2 = preg_replace ($regex6, $change, $url, 1);
-            $url3 = $url."-".$count;
-            $data = getHTML ($url3,30);
+        $change = '';
+        while($pages<70) {
+            sleep(1);
+            $data = getHTML ($url,100);
             $alldata = $alldata.$data;
-            //print ($url3)."<br>";
+            preg_match_all($regex4, $data, $pagination, PREG_SET_ORDER, 0);
+            print (sizeof($pagination[0]))."<br>";
+            print $pages."<br>";
             
+            if (sizeof($pagination[0])===0) {
+                break;
+            }
+            $pages++;
+            $url2 = preg_replace ($regex6, $change, $url);
+            //print $url."<br>";
+            //print $url2."<br>";
+            $url = $url2."-".$pages;
+            print $url."<br>";
         }
-        //print_r($alldata);
+        
         preg_match_all ($regex1,$alldata,$datax);
         
         render("output.php",["datax" => $datax,"regex2" => $regex2,"regex3" => $regex3,"regex5" => $regex5,"regex4" => $regex4,"regex7" => $regex7]);
